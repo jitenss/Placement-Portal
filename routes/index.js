8d131ba74045a9281b062b9cdda85ada14a5194b
jitenss <sadhwani.jiten@gmail.com>
2017-12-18 18:16:48 +0530

    layout done

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-
-
 //Get Homepage
 router.get('/',function(req,res){
-	res.render('login_signup');
+	res.render('login_signup',{layout: 'layout_login_signup'});
 	console.log('home page started');
 });
 
